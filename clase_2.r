b81e779cd380bdc5b7e1cb8ce037eabc88fafbb9
nanshito <106019113+nanshito@users.noreply.github.com>
2023-04-13 16:38:12 -0500

    Agregar archivo clase_2.r

---
+++ clase_2.r
@@ -0,0 +1,6 @@
+if(!require(tydiverse)){install.packages('tydiverse')}
+if(!require(knitr)){install.packages('kinitr')}
+if(!require(KableExtra)){install.packages('KableExtra')}
+library(tydiverse)
+library(knitr)
+library(KableExtra)
