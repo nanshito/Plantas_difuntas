a04cedbf9ce43c5ed1c0a888a3561eb284d1a89b
nanshito <106019113+nanshito@users.noreply.github.com>
2023-04-17 12:07:19 -0500

    Delete clase_2.r

--- clase_2.r
+++
@@ -1,6 +0,0 @@
-if(!require(tydiverse)){install.packages('tydiverse')}
-if(!require(knitr)){install.packages('kinitr')}
-if(!require(KableExtra)){install.packages('KableExtra')}
-library(tydiverse)
-library(knitr)
-library(KableExtra)
